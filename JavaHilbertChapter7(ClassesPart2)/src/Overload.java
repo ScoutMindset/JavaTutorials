3f33396ef0f2311b64b97559168a2fbd9c892773
jaturul <jaturul@gmail.com>
2016-01-26 13:57:21 +0100

    Simple overloading example included

---
+++ JavaHilbertChapter7(ClassesPart2)/src/Overload.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jaturul on 26.01.16.
+ */
+public class Overload
+{
+    public static void main(String args[])
+    {
+        OverloadDemo overloadExample = new OverloadDemo();
+
+        overloadExample.test();
+        overloadExample.test(2.0);
+        overloadExample.test(3);
+    }
+}
---
+++ JavaHilbertChapter7(ClassesPart2)/src/OverloadDemo.java
@@ -0,0 +1,20 @@
+/**
+ * Created by jaturul on 26.01.16.
+ */
+public class OverloadDemo
+{
+    void test()
+    {
+        System.out.println("This is the message of the empty test() function");
+    }
+
+    void test(int a)
+    {
+        System.out.println("This is the message of the integer test() function\n a = " + a);
+    }
+
+    void test(double b)
+    {
+        System.out.println("This is the message of the double test() function\n b = " + b);
+    }
+}
